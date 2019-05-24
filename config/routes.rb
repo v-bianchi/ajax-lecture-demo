0c8859df8290ae8ee50e7efae7f231bf3755d9f6
Victor Bianchi <vbianchi90@gmail.com>
2019-05-24 08:59:22 -0400

    add review tp restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
-  resources :restaurants, only: [ :index, :show ]
+  resources :restaurants, only: [ :index, :show ] do
+    resources :reviews, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
